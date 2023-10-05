97a6f9ac7f1c81e50aaef0aac59948d4d70bdaaa
ErinnS <esummers@appdev.yearup.org>
2023-10-05 15:52:04 -0400

    SandwichShop.java

---
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,29 @@
+package com.pluralsight;
+
+import java.util.*;
+
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println(" What size sandwich would you like ( Regular or Large) ?  ");
+        double Regular = 5.45;
+        double Large = 8.95;
+        String command = myScanner.nextLine();
+        String regular = "Regular";
+        System.out.println(" How old are you? ");
+        myScanner.nextInt();
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
