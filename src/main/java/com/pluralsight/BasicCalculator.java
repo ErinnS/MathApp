b68a618aa88582a42d813134f9315cac5792d1fb
ErinnS <esummers@appdev.yearup.org>
2023-10-05 13:06:08 -0400

    BasicCalculator.java

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter number 1: ");
+        int num_1 = myScanner.nextInt();
+
+        System.out.print("Enter number 2: ");
+        int num_2 = myScanner.nextInt();
+        myScanner.nextLine();
+
+        System.out.println("Possible calculations: ");
+        System.out.print("\n(A)dd \n(S)ubtract \n(M)ultiply \n(D)ivide ");
+        String action = myScanner.nextLine();
+        System.out.println(" Please select an option");
+        int option = myScanner.nextInt();
+
+    }
+
+
+
+
+
+}
